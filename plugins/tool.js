e270b350a8038bd5fa1233c8433be60a8f9cc6dd
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-05-07 02:55:25 +0530

    Update tool.js

--- plugins/tool.js
+++ plugins/tool.js
@@ -10,25 +10,7 @@ Jarvis - Loki-Xer
 ------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 const { System, sendAlive, setData, getData, isPrivate, config } = require("../lib/");  
-
-function getUptime() {
-    const duration = process.uptime();
-    const seconds = Math.floor(duration % 60);
-    const minutes = Math.floor((duration / 60) % 60);
-    const hours = Math.floor((duration / (60 * 60)) % 24);
-    return `_*Runtime: ${hours.toString().padStart(2, "0")} hours ${minutes.toString().padStart(2, "0")} minutes ${seconds.toString().padStart(2, "0")} seconds*_`;
-}
-
-async function Runtime(date) { 
-    const deployedTime = new Date(date);
-    const currentTime = new Date();
-    const runtimeMilliseconds = currentTime - deployedTime;
-    const days = Math.floor(runtimeMilliseconds / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((runtimeMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((runtimeMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((runtimeMilliseconds % (1000 * 60)) / 1000);
-    return `_*Runtime: ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds....*_`
-}
+const { getUptime, Runtime } = require("./client/"); 
 
 System({
 	pattern: "ping",
