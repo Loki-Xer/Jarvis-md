fa3dff2189f1568714a1d70896ba50aec847f0f3
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-05-30 13:00:38 +0530

    Update tools.js

--- plugins/client/tools.js
+++ plugins/client/tools.js
@@ -30,7 +30,7 @@ function secondsToHms(d) {
 
 function selectStyle(styles, index) {
     return new Promise((resolve, reject) => {
-      const numericIndex = parseInt(index);
+      const numericIndex = parseInt(index - 1);
         if (!isNaN(numericIndex) && numericIndex >= 0 && numericIndex < styles.length) {
             resolve(styles[numericIndex]);
         } else {
