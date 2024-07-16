b08092f57cea39740d299845bd54b19a66c53e5c
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-07-16 19:49:49 +0530

    Add files via upload

---
+++ plugins/anime.js
@@ -0,0 +1,48 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Copyright (C) 2023 Loki - Xer.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Jarvis - Loki-Xer 
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+const { System, IronMan, isPrivate, getJson } = require("../lib/");
+
+System({ 
+    pattern: "waifu", 
+    fromMe: isPrivate, 
+    desc: "Send a waifu image", 
+    type: "anime" 
+}, async (message) => {
+    const response = await getJson(await IronMan("ironman/waifu"));
+    if (!response.status) return await message.send("_*Failed to fetch image*_");
+    await message.send(response.ironman.url, { caption: "*here is your waifu*", quoted: message.data }, "image");
+});
+
+System({ 
+    pattern: "neko", 
+    fromMe: isPrivate, 
+    desc: "Send Neko images", 
+    type: "anime" 
+}, async (message) => {
+    const response = await getJson(await IronMan("ironman/neko"));
+    if (!response.status) return await message.send("Failed to fetch image");
+    await message.send(response.ironman.url, { caption: "*here is your neko*", quoted: message.data }, "image");
+});
+
+System({
+    pattern: 'aquote ?(.*)',
+    fromMe: isPrivate,
+    desc: 'Get a random anime quote',
+    type: 'anime',
+}, async (message, match, m) => {
+    const data = await getJson(IronMan('api/aquote'));
+    if (!data && !data.result && !data.result.length > 0) return await message.reply('_*No quotes found.*_');
+    const randomIndex = Math.floor(Math.random() * data.result.length);
+    const { english: enquote, character, anime } = data.result[randomIndex];
+    await message.send(`*➭QUOTE:* ${enquote}\n*➭CHARACTER:* ${character}\n*➭ANIME:* ${anime}`);
+});
