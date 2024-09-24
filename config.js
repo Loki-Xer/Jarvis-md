bf71825e825f25268316aeb41d02d433c043827d
Ragnorgbot31 <fanish2277@gmail.com>
2024-09-24 23:10:13 +0530

    Jarves md config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
   ANTIBOT_MSG: process.env.ANTIBOT_MSG || "_*Bot Not allowed in this group*_",
   ANTIWORD_MSG: process.env.ANTIWORD_MSG || "_*antiword deleted successfully*_",
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_dc0d_6c73_8723_f460_6731_bc18_55c3_d33b',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_e921_1ac4_5ba4_4215_2b43_d763_59d9_4673',
   LANG: process.env.LANG || 'EN',
   SETVV: process.env.SETVV || 'DM',
   ELEVENLABS: process.env.ELEVENLABS,
